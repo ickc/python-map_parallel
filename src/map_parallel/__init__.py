327b14c047ba5fb250d32d9f65d6ea90df00ce68
Kolen Cheung <christian.kolen@gmail.com>
2020-11-19 19:29:16 -0800

    implement map_parallel/starmap_parallel

--- src/map_parallel/__init__.py
+++ src/map_parallel/__init__.py
@@ -1,1 +1,93 @@
 __version__ = '0.1'
+from typing import Optional
+
+
+def _starfunc(f, x):
+    '''return f(*x)
+    '''
+    return f(*x)
+
+
+def map_parallel(
+    f, *args,
+    mode: str = 'multiprocessing',
+    processes: Optional[int] = None,
+):
+    '''equiv to map(f, *args) but in parallel
+
+    :param str mode: backend for parallelization
+
+    - multiprocessing: using multiprocessing from standard library
+    - multithreading: using multithreading from standard library
+    - mpi: using mpi4py.futures. May not work depending on your MPI vendor
+    - serial: using map
+
+    :param int processes: no. of parallel processes
+    (in the case of mpi, it is determined by mpiexec/mpirun args)
+    '''
+    if processes is not None and processes == 1:
+        result = list(map(f, *args))
+    elif mode == 'multiprocessing':
+        from concurrent.futures import ProcessPoolExecutor
+
+        with ProcessPoolExecutor(max_workers=processes) as process_pool_executor:
+            result = list(process_pool_executor.map(f, *args))
+    elif mode == 'multithreading':
+        from concurrent.futures import ThreadPoolExecutor
+
+        with ThreadPoolExecutor(max_workers=processes) as thread_pool_executor:
+            result = list(thread_pool_executor.map(f, *args))
+    elif mode == 'mpi':
+        from mpi4py.futures import MPIPoolExecutor
+
+        with MPIPoolExecutor() as mpi_pool_executor:
+            result = mpi_pool_executor.map(f, *args)
+    else:
+        # fallback
+        result = list(map(f, *args))
+    return result
+
+
+def starmap_parallel(
+    f, args,
+    mode: str = 'multiprocessing',
+    processes: Optional[int] = None,
+):
+    '''equiv to starmap(f, args) but in parallel
+
+    :param str mode: backend for parallelization
+
+    - multiprocessing: using multiprocessing from standard library
+    - multithreading: using multithreading from standard library
+    - mpi: using mpi4py.futures. May not work depending on your MPI vendor
+    - serial: using map
+
+    :param int processes: no. of parallel processes
+    (in the case of mpi, it is determined by mpiexec/mpirun args)
+    '''
+    if processes is not None and processes == 1:
+        from itertools import starmap
+
+        result = list(starmap(f, args))
+    elif mode == 'multiprocessing':
+        from functools import partial
+        from concurrent.futures import ProcessPoolExecutor
+
+        with ProcessPoolExecutor(max_workers=processes) as process_pool_executor:
+            result = list(process_pool_executor.map(partial(_starfunc, f), args))
+    elif mode == 'multithreading':
+        from functools import partial
+        from concurrent.futures import ThreadPoolExecutor
+
+        with ThreadPoolExecutor(max_workers=processes) as thread_pool_executor:
+            result = list(thread_pool_executor.map(partial(_starfunc, f), args))
+    elif mode == 'mpi':
+        from mpi4py.futures import MPIPoolExecutor
+
+        with MPIPoolExecutor() as mpi_pool_executor:
+            result = mpi_pool_executor.starmap(f, args)
+    else:
+        # fallback
+        from itertools import starmap
+        result = list(starmap(f, args))
+    return result
--- tests/test_map_parallel.py
+++ tests/test_map_parallel.py
@@ -1,6 +1,31 @@
+from itertools import starmap
 
-from map_parallel import main
+from map_parallel import map_parallel, starmap_parallel
+
+ARGS = [
+    [3, 4, 5],
+    [5, 12, 13],
+    [1, 2, 3]
+]
 
 
-def test_main():
-    pass
+def f(x, y, z):
+    return x * x + y * y == z * z
+
+
+def test_map_parallel():
+    args = list(map(list, zip(*ARGS)))
+    truth = list(map(f, *args))
+    # MPI is difficult to test
+    for mode in ('multiprocessing', 'multithreading'):
+        for processes in (None, 1, 2):
+            assert map_parallel(f, *args, mode=mode, processes=processes) == truth
+
+
+def test_starmap_parallel():
+    args = ARGS
+    truth = list(starmap(f, args))
+    # MPI is difficult to test
+    for mode in ('multiprocessing', 'multithreading'):
+        for processes in (None, 1, 2):
+            assert starmap_parallel(f, args, mode=mode, processes=processes) == truth
